6c248dbafe378e1a3f2ffabc79e04b48809d3f24
ADI201998 <30969436+ADI201998@users.noreply.github.com>
2019-10-10 19:34:18 +0530

    Add files via upload

--- StepperControl/StepperControl.cpp
+++ StepperControl/StepperControl.cpp
@@ -1,15 +1,21 @@
 #include "StepperControl.h"
-#include "Serial.h"
 
-StepperControl :: StepperControl() : precision{
+StepperControl :: StepperControl(int coil1P, int coil1N, int coil2P, int coil2N) : precision{
     {1,0,0,0}, {1,1,0,0,}, {0,1,0,0}, {0,1,1,0},
     {0,0,1,0}, {0,0,1,1}, {0,0,0,1}, {1,0,0,1}} {
-    //this->get_angle(90);
+    this->coil1P = coil1P;
+    this->coil1P = coil1N;
+    this->coil1P = coil2P;
+    this->coil1P = coil2N;
+    pinMode(coil1P, OUTPUT);
+    pinMode(coil1N, OUTPUT);
+    pinMode(coil2P, OUTPUT);
+    pinMode(coil2N, OUTPUT);
     
 }
 
-void StepperControl :: getSerial()  {
-    while(!Serial)  System.println("Serial not initialized");
+int StepperControl :: getSerial()  {
+    while(!Serial)  Serial.println("Serial not initialized");
 
 }
 
@@ -17,36 +23,39 @@ int StepperControl :: getAngle(int angle)  {
     
 }
 
-void StepperControl :: rotateClockwise(int angle, int coil1P, int coil1N, int coil2P, int coil2N)  {
+void StepperControl :: rotateClockwise(int delay, int serialData)  {
     int j = 0;
-    final_angle = angle/0.6;
+    int final_angle = serialData/0.6;
     for(int i=0; i<final_angle; i++) {
-        digitalWrite(coil1P, arr[j][0]);
-        digitalWrite(coil2P, arr[j][1]);
-        digitalWrite(coil1N, arr[j][2]);
-        digitalWrite(coil2N, arr[j][3]);
+        digitalWrite(coil1P, precision[j][0]);
+        digitalWrite(coil2P, precision[j][1]);
+        digitalWrite(coil1N, precision[j][2]);
+        digitalWrite(coil2N, precision[j][3]);
+        delayMicroseconds(delay);
         j = j<7?j++:0;
     }
 }
 
-void StepperControl :: rotateAnticlockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N)  {
+void StepperControl :: rotateAnticlockwise(int delay, int serialData)  {
     int j = 0;
-    final_angle = serialData/0.6;
+    int final_angle = serialData/0.6;
     for(int i=0; i<final_angle; i++) {
-        digitalWrite(coil2N, arr[j][3]);
-        digitalWrite(coil1N, arr[j][2]);
-        digitalWrite(coil2P, arr[j][1]);
-        digitalWrite(coil1P, arr[j][0]);
-        j = j!=8?j++:0;
+        digitalWrite(coil2N, precision[j][3]);
+        digitalWrite(coil1N, precision[j][2]);
+        digitalWrite(coil2P, precision[j][1]);
+        digitalWrite(coil1P, precision[j][0]);
+        delayMicroseconds(delay);
+        j = j!=7?j++:0;
     }
 }
 
-void StepperControl :: rotate(char dir, int serialPort, int coil1P, int coil1N, int coil2P , int coil2N ) {
-    if(dir == 'c')   rotateClockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N);
-    else if(dir == 'a')  rotateAnticlockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N);
-    else System.println("Enter 'a' or 'c'");
+void StepperControl :: rotate(char dir, int serialPort) {
+    this->coil2P = coil2P;
+    if(dir == 'c')   rotateClockwise(serialData);
+    else if(dir == 'a')  rotateAnticlockwise(serialData);
+    else Serial.println("Enter 'a' or 'c'");
 }
 
-void StepperControl :: rotateToAngle(int serialData, int coil1P = 4, int coil1N = 5, int coil2P = 6, int coil2N = 7)    {
+void StepperControl :: rotateToAngle(int serialData)    {
 
 }

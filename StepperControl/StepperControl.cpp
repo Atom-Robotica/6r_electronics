db04d4daf04579a24b1ba4faee5b91aae8024a19
ADI201998 <30969436+ADI201998@users.noreply.github.com>
2019-10-26 19:37:23 +0530

    Stepper Control with feedback from accelerometer
    Not tested yet. Getting angles from accelerometer properly

--- StepperControl/StepperControl.cpp
+++ StepperControl/StepperControl.cpp
@@ -1,52 +1,85 @@
 #include "StepperControl.h"
-#include "Serial.h"
 
-StepperControl :: StepperControl() : precision{
+StepperControl :: StepperControl(int coil1P, int coil1N, int coil2P, int coil2N) : precision{
     {1,0,0,0}, {1,1,0,0,}, {0,1,0,0}, {0,1,1,0},
     {0,0,1,0}, {0,0,1,1}, {0,0,0,1}, {1,0,0,1}} {
-    //this->get_angle(90);
+    this->coil1P = coil1P;
+    this->coil1P = coil1N;
+    this->coil1P = coil2P;
+    this->coil1P = coil2N;
+    this->index = 0;
+    this->angle = 0;
+    this->cur_angle = 0;
+    pinMode(coil1P, OUTPUT);
+    pinMode(coil1N, OUTPUT);
+    pinMode(coil2P, OUTPUT);
+    pinMode(coil2N, OUTPUT);
     
 }
 
-void StepperControl :: getSerial()  {
-    while(!Serial)  System.println("Serial not initialized");
+int StepperControl :: getSerial()  {
+    while(!Serial)  Serial.println("Serial not initialized");
 
 }
 
-int StepperControl :: getAngle(int angle)  {
-    
+int StepperControl :: getAngle(int X, int Y)  {
+    XVal = analogRead(X) - 346;
+    YVal = analogRead(Y) - 340;
+    float angle = atan2(YVal, XVal);
+    angle = angle*180/3.1415926;
+    if(angle<0)
+        angle = 360 - abs(angle);
+    return int(angle);
 }
 
-void StepperControl :: rotateClockwise(int angle, int coil1P, int coil1N, int coil2P, int coil2N)  {
-    int j = 0;
-    final_angle = angle/0.6;
+void StepperControl :: rotateAnticlockwise(int delay, int serialData)  {
+    int j = index;
+    int final_angle = serialData/0.6;
     for(int i=0; i<final_angle; i++) {
-        digitalWrite(coil1P, arr[j][0]);
-        digitalWrite(coil2P, arr[j][1]);
-        digitalWrite(coil1N, arr[j][2]);
-        digitalWrite(coil2N, arr[j][3]);
+        digitalWrite(coil1P, precision[j][0]);
+        digitalWrite(coil2P, precision[j][1]);
+        digitalWrite(coil1N, precision[j][2]);
+        digitalWrite(coil2N, precision[j][3]);
+        delayMicroseconds(delay);
+        index = j;
+
         j = j<7?j++:0;
     }
+    int cur_angle = final_angle;
+    digitalWrite(coil1P, 0);
+    digitalWrite(coil2P, 0);
+    digitalWrite(coil1N, 0);
+    digitalWrite(coil2N, 0);
 }
 
-void StepperControl :: rotateAnticlockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N)  {
-    int j = 0;
-    final_angle = serialData/0.6;
-    for(int i=0; i<final_angle; i++) {
-        digitalWrite(coil2N, arr[j][3]);
-        digitalWrite(coil1N, arr[j][2]);
-        digitalWrite(coil2P, arr[j][1]);
-        digitalWrite(coil1P, arr[j][0]);
-        j = j!=8?j++:0;
+void StepperControl :: rotateClockwise(int delay, int serialData)  {
+    int j = index;
+    int final_angle = serialData/0.6;
+    for(int i=final_angle; i>0; i++) {
+        digitalWrite(coil1P, precision[j][0]);
+        digitalWrite(coil2P, precision[j][1]);
+        digitalWrite(coil1N, precision[j][2]);
+        digitalWrite(coil2N, precision[j][3]);
+        delayMicroseconds(delay);
+        index = j;
+        j = j!=0?j--:7;
     }
+    digitalWrite(coil1P, 0);
+    digitalWrite(coil2P, 0);
+    digitalWrite(coil1N, 0);
+    digitalWrite(coil2N, 0);
 }
 
-void StepperControl :: rotate(char dir, int serialPort, int coil1P, int coil1N, int coil2P , int coil2N ) {
-    if(dir == 'c')   rotateClockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N);
-    else if(dir == 'a')  rotateAnticlockwise(int serialData, int coil1P, int coil1N, int coil2P, int coil2N);
-    else System.println("Enter 'a' or 'c'");
+void StepperControl :: rotate(char dir, int serialPort) {
+    this->coil2P = coil2P;
+    if(dir == 'c')   rotateClockwise(serialData);
+    else if(dir == 'a')  rotateAnticlockwise(serialData);
+    else Serial.println("Enter 'a' or 'c'");
 }
 
-void StepperControl :: rotateToAngle(int serialData, int coil1P = 4, int coil1N = 5, int coil2P = 6, int coil2N = 7)    {
-
+void StepperControl :: rotateToAngle(int delay, int serialData, int X, int Y)    {
+    if(serialData>getAngle(X, Y))   
+        rotateClockwise(int delay, abs(serialData-getAngle(X, Y)));
+    else if(erialData<getAngle(X, Y))
+        rotateAnticlockwise(int delay, abs(serialData-getAngle(X, Y));
 }
